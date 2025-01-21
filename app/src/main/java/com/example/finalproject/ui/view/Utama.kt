bf0a77c497239df4a814ca8eed8a258e1d57a61a
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2025-01-21 15:51:05 +0700

    membuat halaman utama

--- app/src/main/java/com/example/finalproject/ui/view/Utama.kt
+++ app/src/main/java/com/example/finalproject/ui/view/Utama.kt
@@ -1,2 +1,158 @@
 package com.example.finalproject.ui.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.finalproject.R
+
+@Composable
+fun Utama(
+    onTanamanButton: () -> Unit,
+    onPekerjaButton: () -> Unit,
+    onAktivitasButton: () -> Unit,
+    onCatatanButton: () -> Unit
+) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.op),
+            contentDescription = "",
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Gray),
+            alpha = 0.3f
+        )
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            MenuCard(
+                imageRes = R.drawable.tanaman,
+                title = "Tanaman",
+                buttonColor = Color.Cyan,
+                onClick = onTanamanButton
+            )
+
+            MenuCard(
+                imageRes = R.drawable.pekerja,
+                title = "Pekerja",
+                buttonColor = Color.Green,
+                onClick = onPekerjaButton
+            )
+
+            MenuCard(
+                imageRes = R.drawable.aktivitas,
+                title = "Aktivitas",
+                buttonColor = Color.Green,
+                onClick = onAktivitasButton
+            )
+
+            MenuCard(
+                imageRes = R.drawable.catatan,
+                title = "Catatan",
+                buttonColor = Color.Green,
+                onClick = onCatatanButton
+            )
+        }
+    }
+}
+
+@Composable
+fun MenuCard(
+    imageRes: Int,
+    title: String,
+    buttonColor: Color,
+    onClick: () -> Unit
+) {
+    Card(
+        shape = RoundedCornerShape(24.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.Magenta)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(imageRes),
+                contentDescription = "$title Image",
+                modifier = Modifier
+                    .size(130.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            )
+
+            Spacer(modifier = Modifier.padding(8.dp))
+
+            Box(
+                modifier = Modifier
+                    .weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.titleMedium,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Button(
+                        onClick = onClick,
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        shape = RoundedCornerShape(8.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
+                    ) {
+                        Text("Masuk", color = Color.DarkGray)
+                    }
+                }
+            }
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun UtamaPreview() {
+//    Utama(
+//        onTanamanButton = {},
+//        onPekerjaButton = {},
+//        onAktivitasButton = {},
+//        onCatatanButton = {}
+//    )
+//}
