c5fb25ced7822c4fad3078b7a12b769b54b85785
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2025-01-21 15:43:04 +0700

    menambah alamt navigasi untuk tanaman

--- app/src/main/java/com/example/finalproject/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/finalproject/navigation/AlamatNavigasi.kt
@@ -5,3 +5,27 @@ interface AlamatNavigasi {
     val titleRes: String
 }
 
+// Tanaman
+object AlamatInsertTanaman : AlamatNavigasi {
+    override val route = "insert_tanaman"
+    override val titleRes = "Insert Tanaman"
+}
+
+object AlamatUpdateTanaman : AlamatNavigasi {
+    override val route = "update_tanaman"
+    override val titleRes = "Update Tanaman"
+    const val ID_TANAMAN = "id_tanaman"
+    val routesWithArg = "$route/{$ID_TANAMAN}"
+}
+
+object AlamatDetailTanaman : AlamatNavigasi {
+    override val route = "detail_tanaman"
+    override val titleRes = "Detail Tanaman"
+    const val ID_TANAMAN = "id_tanaman"
+    val routesWithArg = "$route/{$ID_TANAMAN}"
+}
+
+object AlamatHomeTanaman : AlamatNavigasi {
+    override val route = "home_tanaman"
+    override val titleRes = "Home Tanaman"
+}
