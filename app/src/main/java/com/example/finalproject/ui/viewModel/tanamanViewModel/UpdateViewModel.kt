cb4fa188ea0be604de2083c9561cb593eb2e7b9f
MuhammadRav <muhammad.rezza.ft22@mail.umy.ac.id>
2025-01-21 15:49:43 +0700

    membuat tanaman update view mmodel

---
+++ app/src/main/java/com/example/finalproject/ui/viewModel/tanamanViewModel/UpdateViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.finalproject.ui.viewModel.tanamanViewModel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finalproject.navigation.AlamatUpdateTanaman
+import com.example.finalproject.repository.TanamanRepository
+import kotlinx.coroutines.launch
+
+class TanamanUpdateViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val tanamanRepository: TanamanRepository
+): ViewModel(){
+    var UpdateUiState by mutableStateOf(InsertUiState())
+    private set
+
+            private val _idTanaman: String = checkNotNull(savedStateHandle[AlamatUpdateTanaman.ID_TANAMAN])
+
+    init {
+        viewModelScope.launch {
+            UpdateUiState = tanamanRepository.getTanamanById(_idTanaman)
+                .toUiStateTanaman()
+        }
+    }
+
+    fun updateInsertTanamanState(insertUiEvent: InsertUiEvent){
+        UpdateUiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun updateTanaman(){
+        viewModelScope.launch {
+            try {
+                tanamanRepository.updateTanaman(_idTanaman, UpdateUiState.insertUiEvent.toTanaman())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
